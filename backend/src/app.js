5955cf162c1c34ce14b14406f2177f315f56c0f2
rafaelbarbos <rafaelbrbsilva@gmail.com>
2025-07-09 22:56:45 -0300

    Configuração inicial do backend

---
+++ backend/src/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Backend is running');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
